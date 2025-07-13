99679a4927ca221beff68ad60b603c6046e43873
k.phanindra reddy <113985082+KPhanindraReddy@users.noreply.github.com>
2025-07-13 12:03:00 +0530

    Create Find duplicate numbers in an array.c

---
+++ Find duplicate numbers in an array.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online//50101
+#include <stdio.h>
+
+int main() {
+int i, n,found=0;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if(arr[i]==arr[j]){
+            printf("duplicate");
+            found=1;
+            break;
+        }
+    }
+}
+if(!found){
+    printf("no duplicate elements ");
+}
+    return 0;
+}
