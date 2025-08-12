fd7bc0dbcfc8d229bc85e9a776a69fcfdf60cc21
k.phanindra reddy <113985082+KPhanindraReddy@users.noreply.github.com>
2025-08-12 12:29:08 +0530

    Create Floyd’s_Triangle.c

---
+++ Floyd’s_Triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int i,j,num=1;
+    for(i=0;i<4;i++){
+        for(j=0;j<=i;j++){
+            printf("%d ",num++);
+        }
+        printf("\n");
+    }
+}
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+*/
