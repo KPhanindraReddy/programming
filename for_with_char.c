35496fcc94b56f764c9a1c31d559a5b192a5f4b0
k.phanindra reddy <113985082+KPhanindraReddy@users.noreply.github.com>
2025-08-13 09:17:14 +0530

    Create for_with_char.c

---
+++ for_with_char.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char a;
+    int n;
+    scanf("%d",&n);
+    for(a='a';a<='a'+n;a++){
+        printf("%c",a);
+    }
+
+    return 0;
+}
