f8e8f613a89c1a70ebd6c29a6721a54056c879b4
k.phanindra reddy <113985082+KPhanindraReddy@users.noreply.github.com>
2025-08-15 23:45:42 +0530

    Create Max_sub_array_sum_with_Kadanes.c

---
+++ Max_sub_array_sum_with_Kadanes.c
@@ -0,0 +1,17 @@
+int maxSubArray(int* nums, int numsSize) {
+int sum=nums[0];
+int res=nums[0];
+for(int  i=1;i<numsSize;i++){
+    if(sum<0){
+        sum=nums[i];
+    }
+    else{
+        sum+=nums[i];
+    }
+    if(sum >res){
+        res=sum;
+    }
+}
+return res;
+
+}
