ccd84cb0098b2677b2716158a1d065628de08fa4
k.phanindra reddy <113985082+KPhanindraReddy@users.noreply.github.com>
2025-08-18 14:18:56 +0530

    Update Stock_Buy_and_Sell.c

--- Blind 75/Stock_Buy_and_Sell.c
+++ Blind 75/Stock_Buy_and_Sell.c
@@ -1,5 +1,40 @@
 //Method:1
-//b method
+#include <stdio.h>
+int max(int a,int b){
+    return (a>b)?a:b;
+}
+int min(int a,int b){
+    return (a>b)?b:a;
+}
+int max_stock(int arr[],int n){
+    int mini=arr[0],maxprof=0;
+    for(int i=1;i<n;i++){
+        int cur_prof=arr[i]-mini;
+        maxprof=max(cur_prof,maxprof);
+        mini=min(mini,arr[i]);
+        
+    }
+    return maxprof;
+}
+
+int main() {
+    int n;
+    printf("enter the array size:");
+    scanf("%d",&n);
+  int arr[n];
+  printf("enter array elements:");
+
+for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}  
+int res=max_stock(arr,n);
+printf("%d",res);
+}
+
+
+
+
+//Method:2
 #include<stdio.h>
 int main(){
     int n,res=0,i,j,max=0;
@@ -21,6 +56,3 @@ int main(){
     
     
 }
-
-
-//Method:2
